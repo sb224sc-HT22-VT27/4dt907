55fbe4a5cdc9ea5b97504139c139a4088b36cfc9
Samuel Fredric Berg <samuelberg.private@gmail.com>
2026-01-29 05:25:00 +0100

    fix: For the non working workflows; Problems with frontend and docker

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,10 +1,10 @@
+/* eslint-env node */
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => {
-    const envFile = loadEnv(mode, process.cwd(), "");
-    const env = { ...process.env, ...envFile };
+    const env = { ...process.env, ...loadEnv(mode, process.cwd(), "") };
 
     return {
         plugins: [tailwindcss(), react()],
